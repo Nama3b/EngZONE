893391f08d0eeddacff81f274b569bcba53f86e1
Lee Thanh Long <76930578+Nama3b@users.noreply.github.com>
2021-09-22 10:21:08 +0700

    Update updateCart.php

--- updateCart.php
+++ updateCart.php
@@ -9,16 +9,14 @@
         $book_qty = $_POST["book_qty"];
     	if(isset($_SESSION["cart"])){
     		$cart = $_SESSION["cart"];
-    		// echo "<pre>";
-    		// print_r($cart);
 
     		if(array_key_exists($id, $cart)){
 	    		if($_POST["book_qty"]){
 		    		$cart[$id] = array(
-						'book_name'=>$cart[$id]["book_name"],
-						'book_img'=>$cart[$id]["book_img"],
-						'book_price'=>$cart[$id]["book_price"],
-						'book_qty'=>$_POST["book_qty"]
+					'book_name'=>$cart[$id]["book_name"],
+					'book_img'=>$cart[$id]["book_img"],
+					'book_price'=>$cart[$id]["book_price"],
+					'book_qty'=>$_POST["book_qty"]
 	    			);
     			} else{
     				unset($cart[$id]);
